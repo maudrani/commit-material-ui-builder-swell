cbf9b6ef308054a7db5852e1f015c6b4393887fa
Mauricio Draniczarek <mauridrani95@gmail.com>
2022-06-27 13:13:22 -0300

    Fix theme scope

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 
+const theme = createTheme();
+
 export default function Layout(props: { children?: any }) {
-  const theme = createTheme();
   return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 }
